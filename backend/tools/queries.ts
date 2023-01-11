400cfb48bdcb8f10c2004a58ac1168055ff614cd
Alberto <alberto.godinez90@gmail.com>
2023-01-11 00:11:52 -0600

    minor change to createUser response

--- backend/tools/queries.ts
+++ backend/tools/queries.ts
@@ -77,7 +77,7 @@ export class Queries {
         if (error) {
           throw error;
         }
-        response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+        response.status(201).send(`User ${name} was successfully added`);
       }
     );
   }
